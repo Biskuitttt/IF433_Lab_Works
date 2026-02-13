84f9dc0fddbf0aebf41ce5c54709c2fb648b4058
Biskuitttt <biskuithiyahiya@gmail.com>
2026-02-13 20:10:25 +0700

    week02: (task 2) create Hero class

--- src/oop_110898_peterjuanpurnama/week02/Hero.kt
+++ src/oop_110898_peterjuanpurnama/week02/Hero.kt
@@ -1,4 +1,30 @@
 package oop_110898_peterjuanpurnama.week02
 
-class Hero {
+class Hero(
+    val name: String,
+    val baseDamage: Int,
+    var hp: Int = 100
+) {
+
+    fun attack(targetName: String) {
+        println("$name menyerang $targetName!")
+    }
+
+    fun takeDamage(damage: Int) {
+        hp -= damage
+
+        // HP tidak boleh minus
+        if (hp < 0) {
+            hp = 0
+        }
+    }
+
+    fun isAlive(): Boolean {
+        return hp > 0
+    }
+
+    fun showStatus() {
+        println("$name HP tersisa: $hp")
+    }
+
 }
