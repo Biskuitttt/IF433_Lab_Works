48a2cad12699519d02c7030be785d71f18b43aa6
Biskuitttt <biskuithiyahiya@gmail.com>
2026-02-13 19:55:21 +0700

    week02: add warning validation in init block

--- src/oop_110898_peterjuanpurnama/week02/Student.kt
+++ src/oop_110898_peterjuanpurnama/week02/Student.kt
@@ -6,6 +6,16 @@ class Student(
     var major: String
 ) {
 
+    init {
+        if (nim.length != 10) {
+            println("WARNING: NIM harus 10 digit!")
+        }
+
+        if (name.isBlank()) {
+            println("WARNING: Nama tidak boleh kosong!")
+        }
+    }
+
     fun displayInfo() {
         println("===== DATA MAHASISWA =====")
         println("NIM   : $nim")
