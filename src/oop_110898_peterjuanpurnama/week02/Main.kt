8e8392f6e2915891892626ba3c5c0a84768e9700
Biskuitttt <biskuithiyahiya@gmail.com>
2026-02-13 20:02:29 +0700

    week02: implement selection logic for constructors

--- src/oop_110898_peterjuanpurnama/week02/Main.kt
+++ src/oop_110898_peterjuanpurnama/week02/Main.kt
@@ -12,10 +12,26 @@ fun main() {
     println("Masukkan Nama:")
     val name = scanner.nextLine()
 
-    println("Masukkan Jurusan:")
-    val major = scanner.nextLine()
+    println("\nPilih Jalur Pendaftaran:")
+    println("1. Sudah memilih jurusan")
+    println("2. Belum memilih jurusan")
+    print("Pilihan: ")
+    val choice = scanner.nextInt()
+    scanner.nextLine()
 
-    val student = Student(nim, name, major)
+    val student: Student
+
+    if (choice == 1) {
+        println("Masukkan Jurusan:")
+        val major = scanner.nextLine()
+
+        println("Masukkan IPK:")
+        val gpa = scanner.nextDouble()
+
+        student = Student(nim, name, major, gpa)
+    } else {
+        student = Student(nim, name)
+    }
 
     println()
     student.displayInfo()
