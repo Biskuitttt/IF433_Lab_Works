3c9f98496a2d43ebad5ebdeed5354c37df556dba
biskuit hiyahiya <biskuithiyahiya@gmail.com>
2026-03-13 10:43:50 +0700

    week06: (task) create smart home interfaces

---
+++ src/oop_110898_peterjuanpurnama/week06/SmartInterfaces.kt
@@ -0,0 +1,25 @@
+package oop_110898_peterjuanpurnama.week06
+
+interface SmartDevice {
+
+    val id: String
+    val name: String
+
+}
+
+interface Switchable {
+
+    fun turnOn()
+    fun turnOff()
+
+}
+
+interface Recordable {
+
+    fun startRecord()
+
+    fun stopRecord() {
+        println("Perekaman dihentikan dan disimpan ke Cloud.")
+    }
+
+}
