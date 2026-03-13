b160ab71d88d200966509284beab3ddfa7b80147
biskuit hiyahiya <biskuithiyahiya@gmail.com>
2026-03-13 10:46:06 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/oop_110898_peterjuanpurnama/week06/SmartHomeHub.kt
+++ src/oop_110898_peterjuanpurnama/week06/SmartHomeHub.kt
@@ -11,9 +11,23 @@ class SmartHomeHub {
     fun turnOffAllSwitches() {
 
         for (device in devices) {
-
             if (device is Switchable) {
                 device.turnOff()
+            }
+        }
+
+    }
+
+    fun activateSecurityMode() {
+
+        for (device in devices) {
+
+            if (device is Recordable) {
+                device.startRecord()
+            }
+
+            if (device is SmartSpeaker) {
+                device.playMusic("Sirine Peringatan")
             }
 
         }
