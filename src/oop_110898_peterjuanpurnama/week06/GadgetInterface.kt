cdb2a9c6285019ee15702c8a89bc529b8dd882b6
biskuit hiyahiya <biskuithiyahiya@gmail.com>
2026-03-13 10:41:04 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_110898_peterjuanpurnama/week06/GadgetInterface.kt
@@ -0,0 +1,17 @@
+package oop_110898_peterjuanpurnama.week06
+
+interface Camera {
+
+    fun takePhoto() {
+        println("Taking photo from Camera")
+    }
+
+}
+
+interface Phone {
+
+    fun takePhoto() {
+        println("Taking photo from Phone")
+    }
+
+}
