2b8c5c97321389104e87329b56a4adb5b50f934a
Biskuitttt <biskuithiyahiya@gmail.com>
2026-03-29 19:46:44 +0700

    week07: (task) test data class copy and sealed class event dispatch

--- src/oop_110898_peterjuanpurnama/week07/Main.kt
+++ src/oop_110898_peterjuanpurnama/week07/Main.kt
@@ -1,29 +1,35 @@
 package oop_110898_peterjuanpurnama.week07
 
 fun main() {
-    DatabaseManager.connect()
 
+    // Singleton + Companion
+    DatabaseManager.connect()
     val client = NetworkClient.create("https://api.umn.ac.id")
     println(client.baseUrl)
 
-}val u1 = User("A", 20)
-val u2 = User("A", 20)
+    // Regular class
+    val u1 = User("A", 20)
+    val u2 = User("A", 20)
+    println(u1)
+    println(u1 == u2)
 
-println(u1)
-println(u1 == u2)
+    // Data class
+    val d1 = UserData("A", 20)
+    val d2 = UserData("A", 20)
+    println(d1)
+    println(d1 == d2)
 
-val d1 = UserData("A", 20)
-val d2 = UserData("A", 20)
+    val d3 = d1.copy(age = 25)
+    val (name, age) = d3
+    println(name)
+    println(age)
 
-println(d1)
-println(d1 == d2)
+    // Test GameManager
+    GameManager.startGame()
+    GameManager.startGame()
+}
 
-val d3 = d1.copy(age = 25)
-val (name, age) = d3
-
-println(name)
-println(age)
-
+// function boleh di luar main
 fun testResponse(res: ApiResponse) {
     val msg = when (res) {
         is ApiResponse.Success -> "OK"
@@ -31,18 +37,4 @@ fun testResponse(res: ApiResponse) {
         ApiResponse.Loading -> "Loading..."
     }
     println(msg)
-
-    GameManager.startGame()
-    GameManager.startGame()
-
-    println(ItemRarity.LEGENDARY.dropChance)
-
-    val weapon = Weapon.forgeStarterSword()
-    println(weapon.item)
-
-    val upgraded = weapon.item.copy(damage = 25)
-
-    processEvent(BattleState.SafeZone)
-    processEvent(BattleState.MonsterEncounter("Goblin Nakal"))
-    processEvent(BattleState.LootDropped(upgraded))
-    processEvent(BattleState.GameOver("Terkena racun"))
+}
