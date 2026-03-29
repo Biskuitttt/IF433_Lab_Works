fc3fde50d0ca2d25dbc936fbc0d045f3cb6f0273
Biskuitttt <biskuithiyahiya@gmail.com>
2026-03-29 19:58:29 +0700

    week07: (task) test data class copy and sealed class event dispatch

--- src/oop_110898_peterjuanpurnama/week07/Main.kt
+++ src/oop_110898_peterjuanpurnama/week07/Main.kt
@@ -1,39 +1,30 @@
 package oop_110898_peterjuanpurnama.week07
 
 fun main() {
-    DatabaseManager.connect()
 
+    DatabaseManager.connect()
     val client = NetworkClient.create("https://api.umn.ac.id")
     println(client.baseUrl)
-}
 
-val u1 = User("A", 20)
-val u2 = User("A", 20)
-println(u1)
-println(u1 == u2)
+    val u1 = User("A", 20)
+    val u2 = User("A", 20)
+    println(u1)
+    println(u1 == u2)
 
-val d1 = UserData("A", 20)
-val d2 = UserData("A", 20)
-println(d1)
-println(d1 == d2)
+    val d1 = UserData("A", 20)
+    val d2 = UserData("A", 20)
+    println(d1)
+    println(d1 == d2)
 
-val d3 = d1.copy(age = 25)
-val (name, age) = d3
-println(name)
-println(age)
-
-fun testResponse(res: ApiResponse) {
-    val msg = when (res) {
-        is ApiResponse.Success -> "OK"
-        is ApiResponse.Error -> "Error"
-        ApiResponse.Loading -> "Loading..."
-    }
+    val d3 = d1.copy(age = 25)
+    val (name, age) = d3
+    println(name)
+    println(age)
 
     GameManager.startGame()
     GameManager.startGame()
 
     println(ItemRarity.LEGENDARY.dropChance)
-
     val weapon = Weapon.forgeStarterSword()
     println(weapon.item)
 
@@ -43,5 +34,13 @@ fun testResponse(res: ApiResponse) {
     processEvent(BattleState.MonsterEncounter("Goblin Nakal"))
     processEvent(BattleState.LootDropped(upgraded))
     processEvent(BattleState.GameOver("Terkena racun"))
+}
 
-    heheheh
+fun testResponse(res: ApiResponse) {
+    val msg = when (res) {
+        is ApiResponse.Success -> "OK"
+        is ApiResponse.Error -> "Error"
+        ApiResponse.Loading -> "Loading..."
+    }
+    println(msg)
+}
