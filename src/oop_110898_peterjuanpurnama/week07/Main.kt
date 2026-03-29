657c04cc079df93d6ca5b6bc62909020a75a139a
Biskuitttt <biskuithiyahiya@gmail.com>
2026-03-29 19:39:03 +0700

    week07: implement data class for structural equality

--- src/oop_110898_peterjuanpurnama/week07/Main.kt
+++ src/oop_110898_peterjuanpurnama/week07/Main.kt
@@ -1,3 +1,5 @@
+package oop_110898_peterjuanpurnama.week07
+
 fun main() {
     DatabaseManager.connect()
 
@@ -9,3 +11,9 @@ val u2 = User("A", 20)
 
 println(u1)
 println(u1 == u2) // false
+
+val d1 = UserData("A", 20)
+val d2 = UserData("A", 20)
+
+println(d1)
+println(d1 == d2) // true
