02976c7386b3abd4ff4de8fe94f63b2d1165c9dd
Biskuitttt <biskuithiyahiya@gmail.com>
2026-04-27 19:42:46 +0700

    week09: create Student data class and list

---
+++ src/oop_110898_peterjuanpurnama/week09/ObjectChaining.kt
@@ -0,0 +1,11 @@
+package oop_110898_peterjuanpurnama.week09
+
+data class Student(val name: String, val score: Int)
+
+fun main() {
+    val students = listOf(
+        Student("A", 80),
+        Student("B", 90),
+        Student("C", 70)
+    )
+}}
