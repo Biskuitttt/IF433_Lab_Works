0484e8a4fa9a1d42b5fe2be8759512242dcf0be1
biskuit hiyahiya <biskuithiyahiya@gmail.com>
2026-05-15 10:43:59 +0700

    week12: (task) handle feeding execution with multiple catch

---
+++ src/oop_110898_peterjuanpurnama/week12/main.kt
@@ -0,0 +1,20 @@
+try {
+
+    currentKibbleStock = dispenseKibble(
+        requestedGram = 80,
+        availableGram = currentKibbleStock,
+        isJammed = false
+    )
+
+} catch (e: DispenserJamException) {
+
+    println(e.message)
+
+} catch (e: FoodEmptyException) {
+
+    println(e.message)
+
+} catch (e: Exception) {
+
+    println(e.message)
+}
